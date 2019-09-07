8ba7029bd1ef1a309cd85614f3d39010428e033b
akuny <andykuny@gmail.com>
2019-09-07 09:39:49 -0400

    Add options to view league spending by year and total club spending

--- public/main.js
+++ public/main.js
@@ -1,36 +1,64 @@
 (function() {
     
+    // state object
     var state = {
         rawDataset: null,
         transformedDataset: null,
         subset: null,
+        isInit: true,
         isLoading: true,
         selectedYear: '2016',
+        selectedOption: 'teamByYear'
     };
 
+    // pull in data
     d3.dsv(',', 'Salaries.csv')
     .then(function(data) {
         state.dataset = data;
         state.isLoading = false;
-        transformData();
-        populateYearDropdown();
-        state.subset = pullSubset();
-        drawChart(state.subset);
+        render();
+        state.isInit = false;
     }).catch(function(err) {
         console.error(err);
     });
+    
+    // event handlers
+    d3.select('#formatOptions')
+    .on('click', function() {
+        if (this.value !== state.selectedOption) {
+            var yearPicker = document.getElementById('yearPicker');
+            if (this.value === 'teamByYear' || this.value === 'leagueByYear') {
+                yearPicker.style.display = 'block';
+            } 
+            if (this.value === 'teamAllYears') {
+                yearPicker.style.display = 'none';
+            }
+            state.selectedOption = this.value
+            render();
+        }
+    });
 
     d3.select('#yearOptions')
-        .on('click', function() {
-            if (this.value !== state.selectedYear) {
-                state.selectedYear = this.value;
-                transformData();
-                eraseChart();
-                state.subset = pullSubset();
-                drawChart(state.subset);
-            }
-        });
+    .on('click', function() {
+        if (this.value !== state.selectedYear) {
+            state.selectedYear = this.value;
+            render();
+        }
+    });
 
+    // render function
+    var render = function() {
+        transformData(state.selectedOption);
+        if (state.isInit) {
+            populateYearDropdown();
+        } else {
+            eraseChart();
+        }
+        state.subset = pullSubset(state.selectedOption);
+        drawChart(state.subset);
+    };
+
+    // functions called by render()
     var populateYearDropdown = function() {
         var dropdown = document.getElementById('yearOptions');
         for (var year = 2016; year >= 1985; year--) {
@@ -38,38 +66,80 @@
             option.text = year;
             option.value = year;
             dropdown.appendChild(option)
-        }
-    }
- 
-    var transformData = function() {
-        state.transformedDataset = d3.rollup(state.dataset, function(v) {
-            return d3.sum(v, function(d) {
-                return d.salary;
-            });
-        }, function(d) {
-            return d.yearID;
-        }, function(d) {
-            return d.teamID;
-        });
-    }
+        };
+    };
 
-    var pullSubset = function() {
-        var yearData = state.transformedDataset.get(state.selectedYear);
+    var transformData = function(option) {
+        switch(option) {
+            case 'teamByYear':
+                state.transformedDataset = d3.rollup(state.dataset, function(v) {
+                    return d3.sum(v, function(d) {
+                        return d.salary;
+                    });
+                }, function(d) {
+                    return d.yearID;
+                }, function(d) {
+                    return d.teamID;
+                });
+                break;
+            case 'teamAllYears':
+                state.transformedDataset = d3.rollup(state.dataset, function(v) {
+                    return d3.sum(v, function(d) {
+                        return d.salary;
+                    });
+                }, function(d) {
+                    return d.teamID;
+                });
+                break;
+            case 'leagueByYear':
+                state.transformedDataset = d3.rollup(state.dataset, function(v) {
+                    return d3.sum(v, function(d) {
+                        return d.salary;
+                    });
+                }, function(d) {
+                    return d.yearID;
+                }, function(d) {
+                    return d.lgID;
+                });
+            default:
+                // console.log(option);
+        };
+    };
 
+    var pullSubset = function(option) {
         var mapToArrOfObj = function(map) {
             var array = [];
             map.forEach(function(value, key) {
                 array.push({team: key, salary: value})
             })
             return array;
-        }
+        };
 
-        return mapToArrOfObj(yearData);
-    }
+        switch(option) {
+            case 'teamByYear':
+            case 'leagueByYear':
+                var yearData = state.transformedDataset.get(state.selectedYear);
+                return mapToArrOfObj(yearData);
+            case 'teamAllYears':
+                return mapToArrOfObj(state.transformedDataset);
+        };
+    };
 
     var drawChart = function(data) {
 
-        document.getElementById('title').innerHTML = state.selectedYear + ' MLB Club Wages'
+        var title = document.getElementById('title')
+        
+        if (state.selectedOption === 'teamByYear') {
+            title.innerHTML = state.selectedYear + ' MLB Club Wages';
+        }
+
+        if (state.selectedOption === 'leagueByYear') {
+            title.innerHTML = state.selectedYear + ' MLB Wages by League';
+        }
+
+        if (state.selectedOption === 'teamAllYears') {
+            title.innerHTML = 'Combined Spending by MLB Club: 1995 - 2016';
+        }
 
         var margin = {
             top: 20,
@@ -77,31 +147,34 @@
             bottom: 30,
             left: 100
         };
-        
+
         var svg = d3.select('svg');
         var width = +svg.attr('width') - margin.left - margin.right;
         var height = +svg.attr('height') - margin.top - margin.bottom;
-        var g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+        var g = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
         var x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
         var y = d3.scaleLinear().rangeRound([height, 0]);
 
-        x.domain(data.map(function(d) { return d.team; }));
+        x.domain(data.map(function(d) {
+            return d.team;
+        }));
 
 	    y.domain([0, d3.max(data, function(d) {
             return d.salary
         })]);
 
         g.append('g')
-            .attr('class', 'axis axis--x')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(d3.axisBottom(x));
+        .attr('class', 'axis axis--x')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(d3.axisBottom(x));
 
         g.append('g')
-            .attr('class', 'axis axis--y')
-            .call(d3.axisLeft(y).ticks(11, data.salary))
-            .append('text')
+        .attr('class', 'axis axis--y')
+        .call(d3.axisLeft(y).ticks(11, data.salary))
+        .append('text')
             .attr('transform', 'rotate(-90)')
             .attr('y', 6)
             .attr('dy', '0.71em')
@@ -121,13 +194,12 @@
             .attr('width', x.bandwidth())
             .attr('height', function (d) {
                 return height - y(d.salary);
-            })
-        ;       
+            });
     }
 
     var eraseChart = function() {
-        d3.select('#canvas').selectAll('*').remove();
-    }
+        d3.select('#screen').selectAll('*').remove();
+    };
 
     // TODO format annual salary for tooltip
     var moneyFormatter = new Intl.NumberFormat('en-US', {
@@ -139,6 +211,6 @@
     window.onbeforeunload = function() {
         var dropdown = document.getElementById('yearOptions');
         dropdown.value = 2016;
-    }
+    };
 
 })();
